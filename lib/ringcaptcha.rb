f92db5762b4624bbfa48e84bd7648a43acb5c7b2
cyb- <gchateau@student.42.fr>
2018-01-20 21:26:41 +0100

    add phone verification and send message methods

--- lib/ringcaptcha.rb
+++ lib/ringcaptcha.rb
@@ -6,32 +6,49 @@ require 'ostruct'
 module RingCaptcha
   class RingCaptchaRequestError < StandardError; end
 
-  class RingCaptchaVerification
-
+  class RingCaptchaResponse
     extend Forwardable
-    PUBLIC_METHODS = [:status, :message, :transaction_id, :phone,  :geolocation, :phone_type, :carrier_name, :roaming, :risk, :json]
 
-    PUBLIC_METHODS.each do |m|
-      def_delegators :@json, m
-    end
+    attr_reader :response
 
-    def initialize(json)
-      @json = OpenStruct.new(json)
+    def initialize(response)
+      @json = JSON.parse(response.body)
+      @datas = OpenStruct.new(as_json)
+      @response = response
+
+      self.class::PUBLIC_METHODS.each do |m|
+        self.class.def_delegators :@datas, m
+      end
     end
 
     def valid?
       status == "SUCCESS"
     end
 
-    def as_json(options={})
-      @json.to_h.slice(*PUBLIC_METHODS)
+    def as_json
+      @json.select{ |k,v| self.class::PUBLIC_METHODS.include?(k.to_sym) }
     end
   end
 
+  class RingCaptchaValidation < RingCaptchaResponse
+    PUBLIC_METHODS = [:status, :message, :id, :phone, :token, :country, :service, :attempt, :pcp, :retry_in, :expires_in, :reason].freeze
+  end
+
+  class RingCaptchaVerification < RingCaptchaResponse
+    PUBLIC_METHODS = [:status, :message, :id, :phone, :dialog_code, :country, :service, :geolocation, :referer].freeze
+  end
+
+  class RingCaptchaMessage < RingCaptchaResponse
+    PUBLIC_METHODS = [:status, :message, :id, :phone, :message_count, :reason].freeze
+  end
+
   class RingCaptcha
+    AVAILABLE_SERVICES = [:sms, :voice].freeze
+
     @@rc_server     = 'api.ringcaptcha.com'
     @@user_agent    = 'ringcaptcha-ruby/1.0'
 
+    attr_reader   :message
     attr_accessor :secure
 
     def initialize(app_key, secret_key)
@@ -39,46 +56,72 @@ module RingCaptcha
       @secret_key = secret_key
       @retry_attempts = 0
       @secure = true
-      @status = -1
+    end
+
+    def secure?
+      @secure
+    end
+
+    def secure=(new_value)
+      @secure = new_value
+    end
+
+    def send_pin_code(phone_number, service = :sms)
+      raise ArgumentError, "undefined service `#{service}', availables are: #{AVAILABLE_SERVICES.inspect}" unless AVAILABLE_SERVICES.include?(service.to_sym)
+      #TODO Check parameters
+      data = { secret_key: @secret_key, phone: phone_number }
+
+      begin
+        response = api_rest_call("#{@app_key}/code/#{service}", data)
+      rescue => e
+        @message = e.message
+      end
+
+      return RingCaptchaValidation.new(response)
     end
 
     def validate_pin_code(pin_code, token)
       #TODO Check parameters
-      data = {:secret_key => @secret_key, :token => token, :code => pin_code}
-      sanitize_data(data)
-      server = (@secure ? "https://" : "http://") + @@rc_server
-      resource = "#{@app_key}/verify"
+      data = { secret_key: @secret_key, token: token, code: pin_code }
+
       begin
-        response = verify_rest_call(server, resource, data)
-        body = JSON.parse(response.body)
-        @status = response.class.name == "Net::HTTPOK" ? body['status'] == "SUCCESS" : 0
+        response = api_rest_call("#{@app_key}/verify", data)
       rescue => e
-        @status = 0
         @message = e.message
       end
 
-      return RingCaptchaVerification.new(body)
+      return RingCaptchaVerification.new(response)
     end
 
-    def is_valid?(pin_code, token)
-      validate_pin_code(pin_code, token)
-      @status == true
+    def send_message(phone_number, message)
+      #TODO Check parameters
+      data = { secret_key: @secret_key, phone: phone_number, message: message }
+
+      begin
+        response = api_rest_call("#{@app_key}/sms", data)
+      rescue => e
+        @message = e.message
+      end
+
+      return RingCaptchaMessage.new(response)
     end
 
     private
 
     def sanitize_data(data)
       data.each do |key,value|
-        data[key] = URI::encode(value).strip
+        data[key] = URI::encode(value).strip unless key == :message
       end
     end
 
-    def verify_rest_call(server, resource, data, port=80)
-      port = @secure ? 443 : port
-      url = "#{server}:#{port}/#{resource}"
+    def api_rest_call(resource, data, port = 80)
+      protocol = @secure ? "https://" : "http://"
+      host = @@rc_server 
+      port = @secure == true ? 443 : port
+      sanitize_data(data)
 
-      uri = URI.parse(url)
-      https = Net::HTTP.new(uri.host,uri.port)
+      uri = URI.parse("#{protocol}#{host}:#{port}/#{resource}")
+      https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = @secure
       req = Net::HTTP::Post.new(uri.path, initheader = {'User-Agent' => @@user_agent})
       req.set_form_data(data)
--- lib/ringcaptcha/version.rb
+++ lib/ringcaptcha/version.rb
@@ -1,3 +1,3 @@
 module RingCaptcha
-  VERSION = "0.2.0"
+  VERSION = "0.2.3.alpha1"
 end
